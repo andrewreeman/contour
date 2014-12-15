35f8d2597a5cb3dd61ecbc3f3db26d6bd1086de2
Andy <andrew.reeman@gmail.com>
2014-12-15 02:15:35 +0000

    loads midi and chooses random note

--- Contour/Form1.Designer.cs
+++ Contour/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.PlayMelody = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(368, 95);
+            this.button1.Location = new System.Drawing.Point(54, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -51,11 +52,22 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
+            // PlayMelody
+            // 
+            this.PlayMelody.Location = new System.Drawing.Point(378, 91);
+            this.PlayMelody.Name = "PlayMelody";
+            this.PlayMelody.Size = new System.Drawing.Size(75, 23);
+            this.PlayMelody.TabIndex = 2;
+            this.PlayMelody.Text = "Play Melody";
+            this.PlayMelody.UseVisualStyleBackColor = true;
+            this.PlayMelody.Click += new System.EventHandler(this.PlayMelody_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(766, 262);
+            this.Controls.Add(this.PlayMelody);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,6 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PlayMelody;
     }
 }
 
