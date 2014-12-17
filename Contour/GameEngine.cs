188f3ff8211c6b6bbb6ce9bbb337561d8ca8674a
Andy <andrew.reeman@gmail.com>
2014-12-17 02:41:33 +0000

    Seperated into three classes

---
+++ Contour/GameEngine.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Contour
+{
+    public struct NoteData
+    {
+        public int NoteValue { get; set; }
+        public int NoteIndex { get; set; }
+    }
+
+    enum NoteDirection
+    {
+        ABOVE, BELOW, SAME
+    }
+
+    class GameEngine
+    {
+        public GameEngine()
+        {
+            //TODO data bindings
+                
+            //TODO filter file list for midi files
+            this.MidiEngine = new MidiEngine();
+                
+            this.MidiFiles = new List<string>(Directory.GetFiles(@"../../media/"));                               
+            this.UserScore = 0;
+        }
+
+
+        private NoteData RandNote()
+        {
+            Random random = new Random();
+            NoteData noteData = new NoteData();
+
+            noteData.NoteIndex = random.Next(1, this.Notes.Count); // can't pick the first note
+            noteData.NoteValue = this.Notes.ElementAt(noteData.NoteIndex);
+            return noteData;
+        }
+            
+        private string RandSong()
+        {
+            Random randGen = new Random();
+            int size = this.MidiFiles.Count;
+            return this.MidiFiles.ElementAt(randGen.Next(size));
+        }
+
+        private int getDirectionFromPrevious()
+        {
+            int previousNoteValue = this.Notes.ElementAt(this.AnswerNote.NoteIndex - 1);
+            if (this.AnswerNote.NoteValue == previousNoteValue)
+            {
+                return (int)NoteDirection.SAME;
+            }
+            else if (this.AnswerNote.NoteValue > previousNoteValue)
+            {
+                return (int)NoteDirection.ABOVE;
+            }
+            else
+            {
+                return (int)NoteDirection.BELOW;
+            }
+        }
+              
+        //TODO test all midi files
+        public void NextGo()
+        {
+            this.FileName = RandSong();
+            this.Notes = this.MidiEngine.GetNoteOn();
+            this.AnswerNote = RandNote();
+            this.CorrectDirection = getDirectionFromPrevious();
+        }
+
+        public void Play()
+        {
+            this.MidiEngine.Play();
+        }
+            
+        public bool isUserCorrect(){
+            if(UserSelected == CorrectDirection){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        public string FileName{
+            get{
+                return this.MidiEngine.FileName;
+            }
+            set{
+                this.MidiEngine.FileName = value;
+            }
+        }
+
+        private List<string> MidiFiles;
+        private List<int> Notes;
+        public NoteData AnswerNote{get; private set;}
+        private int CorrectDirection { get; set; }
+        public int UserSelected {get; set;}            
+        public int UserScore { get; set; } //TODO to bind
+        private MidiEngine MidiEngine;
+    }
+}
+
---
+++ Contour/MidiEngine.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanford.Multimedia.Midi;
+
+
+namespace Contour
+{
+    
+    class MidiEngine
+    {
+        public MidiEngine()
+        {
+            this.Output = new OutputDevice(0);
+            this.Sequencer = new Sequencer();
+            this.Sequencer.ChannelMessagePlayed += this.onChannel;
+            this.Sequencer.PlayingCompleted += this.onPlayingCompleted;       
+        }
+
+        public void onChannel(object sender, ChannelMessageEventArgs args)
+        {
+            this.Output.Send(args.Message);
+        }
+    
+        public void onPlayingCompleted(object sender, EventArgs args)
+        {
+            this.isPlaying = false;
+        }
+
+        public List<int> GetNoteOn()
+        {
+            List<int> notes = new List<int>();
+            foreach (Track melody in this.Sequence)
+            {
+                foreach (MidiEvent ev in melody.Iterator())
+                {
+                    IMidiMessage mess = ev.MidiMessage;
+                    if (mess.MessageType == MessageType.Channel)
+                    {
+                        ChannelMessage channelMess = (ChannelMessage)mess;
+                        if (channelMess.Command == ChannelCommand.NoteOn)
+                        {
+                            notes.Add(channelMess.Data1);
+                        }
+                    }
+                }
+            }
+            return notes;
+        }
+
+        public void Play()
+        {
+            if (this.isPlaying == false)
+            {
+                this.Sequencer.Start();
+                this.isPlaying = true;
+            }
+        }
+    //TODO visual feedback on which note
+        private string fileName;
+        public string FileName { 
+            get{
+                return fileName;
+            } 
+            set{
+                fileName = value;
+                this.isPlaying = false;
+                this.Sequencer.Sequence = this.Sequence = new Sequence(value);
+            } 
+        } //TODO to bind
+        private OutputDevice Output { get; set; }
+        private Sequencer Sequencer { get; set; }
+        private Sequence Sequence { get; set; }
+        private bool isPlaying;   
+    }
+}
+
